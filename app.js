6b1892c292e2af125dfa72102d2b5d94841bb966
uzairmirza <53081780+uzairmirza90@users.noreply.github.com>
2022-04-11 17:21:11 +0500

    Delete Card Functionality Added

--- app.js
+++ app.js
@@ -9,16 +9,26 @@ document.getElementById('popular').addEventListener('click', () => renderPopular
 document.getElementById('home').addEventListener('click', () => renderHome())
 
 
+//      Global Variables
+let first20UpcomingElements = []
+let popularElements = []
+
+
+
 //      Function to fetch upcoming api data
 const fetchUpcomingApi = async () => {
     try {
         let apiResponse = await fetch(upcomingApiLink)
         let json = await apiResponse.json()
-        return json
+        const { data } = json
+        first20UpcomingElements = data.slice(0, 20)
+        console.log(first20UpcomingElements)
     } catch (e) {
         console.log(e)
     }
 }
+
+fetchUpcomingApi()
 
 
 //      Function to fetch popular api data
@@ -26,44 +36,45 @@ const fetchPopularApi = async () => {
     try {
         let apiResponse = await fetch(popularApiLink)
         let json = await apiResponse.json()
-        return json
+        const { entries } = json
+        popularElements = entries
     } catch (e) {
         console.log(e)
     }
 }
 
+fetchPopularApi()
+
+
 
 //      Function to render Upcoming data from api
 async function renderUpcomingData() {
-    let apiData = await fetchUpcomingApi()
     let renderHTML = '';
-    console.log(apiData)
-    const { lastUpdate, lanuage, data } = apiData;
-    let first20Elements = data.slice(0, 20)
-    first20Elements.map((element, index) => {
+    first20UpcomingElements.map((element, index) => {
         const { item } = element;
-        let html = `<div class="card-item">
+        let html = `<div class="card-item" >
                         <img src=${item.images.icon} class="card-image">
                         <h4>Name: ${item.name}</h4>
                         <h4>Type: ${item.type}</h4>
                         <h4>Rarity: ${item.rarity}</h4>
                         <h4>Series: ${item.series}</h4>
                         <h4>Upcoming: ${item.upcoming}</h4>
+                        <button onclick="deleteCard(${index})">Delete</button>
                     </div>`;
 
         renderHTML += html;
     })
 
     document.getElementById('card').innerHTML = renderHTML
+   
 }
+
 
 
 //      Function to render Popular data from api
 async function renderPopularData() {
-    let apiData = await fetchPopularApi()
     let renderHTML = '';
-    const { lastUpdate, lanuage, entries } = apiData;
-    entries.map((element, index) => {
+    popularElements.map((element, index) => {
         const { name, type, entries } = element
         entries.map((el) => {
             const { name, description, serie, type, images } = el;
@@ -81,6 +92,16 @@ async function renderPopularData() {
         document.getElementById('card').innerHTML = renderHTML
     })
 }
+
+
+
+//      Function to delete Card
+function deleteCard(index){
+    let filteredArray = first20UpcomingElements.filter((item, i) => i !== index)
+    first20UpcomingElements = filteredArray
+    renderUpcomingData()
+}
+
 
 
 //      Function to render Home Data
